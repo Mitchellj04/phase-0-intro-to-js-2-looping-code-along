6e85206229723d957b9a3b159f562c29287cf429
JustinMitchell <mitchelljm04@gmail.com>
2022-01-30 21:19:37 -0500

    commit tree a35ca7e0594c5ab759c332ad8b5873fcb9d8547f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const eventName = ["wonderful"];
+
+function writeCards(names, eventName) {
+    const thankYou = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYou[i] = `Thank you, ${names[i]}, for the ${eventName} gift!`
+        debugger;
+    }
+    return thankYou;
+}
